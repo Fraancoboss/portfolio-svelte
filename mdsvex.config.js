a3430a4392d59cf20ac0c3f725d9f16fef14c61b
Fran <fraancoboss@gmail.com>
2025-11-08 17:45:14 +0000

    COnfiguera PAges

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,7 +1,13 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, resolve } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('mdsvex').MdsvexOptions} */
 const config = {
 	extensions: ['.md'],
-	layout: '$lib/components/PostLayout.svelte',
+	layout: resolve(__dirname, 'src/lib/components/PostLayout.svelte'),
 	smartypants: {
 		quotes: true,
 		dashes: 'oldschool'
