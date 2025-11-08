1d79285dd714bdaa2d7b28bc73174a0877259f3a
Fran <fraancoboss@gmail.com>
2025-11-08 17:29:47 +0000

    Configuración de github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', ...mdsvexConfig.extensions],
-	preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
-	kit: {
-		adapter: adapter()
-	}
+  extensions: ['.svelte', ...mdsvexConfig.extensions],
+  preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/portfolio-svelte' : ''
+    }
+  }
 };
 
 export default config;
