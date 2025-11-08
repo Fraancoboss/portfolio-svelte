dc420d60de76e0f874c6c450d939faf4739f6f96
Fran <fraancoboss@gmail.com>
2025-11-08 17:36:41 +0000

    svelte.config.js sintax problem solved

--- svelte.config.js
+++ svelte.config.js
@@ -3,20 +3,21 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: ['.svelte', ...mdsvexConfig.extensions],
-  preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: undefined,
-      precompress: false
-    }),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/portfolio-svelte' : ''
-    }
-  }
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/portfolio-svelte' : ''
+		}
+	}
 };
 
 export default config;
