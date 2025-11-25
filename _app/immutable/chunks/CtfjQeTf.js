fe3776cf9d9941c49b3f70ca6a34db8806844b22
Fran <fraancoboss@gmail.com>
2025-11-25 20:52:30 +0000

    Updates

---
+++ _app/immutable/chunks/CtfjQeTf.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_2gk8dt?.base??"/portfolio-svelte",t=globalThis.__sveltekit_2gk8dt?.assets??s??"";export{t as a,s as b};
---
+++ _app/immutable/chunks/DeNHWR2b.js
@@ -0,0 +1,1 @@
+import{g as p,E as t}from"./DE_jnxu_.js";import{B as c}from"./C3v6Y4IS.js";function E(r,s,...a){var e=new c(r);p(()=>{const n=s()??null;e.ensure(n,n&&(o=>n(o,...a)))},t)}export{E as s};
